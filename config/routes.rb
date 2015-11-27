fa3dd81b5bf19b2b61199168555974c8417d7645
cattoa <allistair@bobcatt.co.za>
2015-11-27 05:36:40 +0200

    Added bootstrap and some boostrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :waitrons
   resources :tables
-  root 'tables#index'
+  #root 'tables#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
