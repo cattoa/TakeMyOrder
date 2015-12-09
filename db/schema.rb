fdb48f6e3cc58ee09d3365d969c61e473c6d56b1
cattoa <allistair@bobcatt.co.za>
2015-12-09 14:37:04 +0000

    Fixing menu pages

--- db/schema.rb
+++ db/schema.rb
@@ -33,13 +33,12 @@ ActiveRecord::Schema.define(version: 20151208222328) do
   end
 
   create_table "menus", force: :cascade do |t|
-    t.string   "name",         limit: 255
+    t.string   "name",        limit: 255
     t.date     "date_from"
     t.date     "date_to"
-    t.text     "description",  limit: 65535
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.integer  "menu_item_id", limit: 4
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "tables", force: :cascade do |t|
